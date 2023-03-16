9542fc73741960d7c08182642a87c5ea6ab7f6fc
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-03-16 23:20:59 +0200

    remove outer loop

--- assignment1/UniformQuantizer.m
+++ assignment1/UniformQuantizer.m
@@ -2,11 +2,7 @@ function q_ind = UniformQuantizer(in_val, n_bits, xmax, m)
     L = 2 .^ n_bits;
     delta = (2 * xmax) / L;
     levels = (m * delta / 2) - xmax:delta:(m * delta / 2) + xmax;
-
-    q_ind = zeros(1, length(in_val));
-
-    for i = 1:length(in_val)
-        q_ind(i) = min(max(floor((in_val(i) + -1*levels(1))/delta) + 1,1),length(levels) - 1);
-    end
+    
+    q_ind = min(max(floor((in_val + -1*levels(1))/delta) + 1,1),length(levels) - 1);
 
 end
