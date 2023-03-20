2f303c027749b25dbc98d72cf7065af08718261f
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-03-20 19:10:42 +0200

    remove levels array from quantizer

--- assignment1/UniformQuantizer.m
+++ assignment1/UniformQuantizer.m
@@ -1,7 +1,6 @@
 function q_ind = UniformQuantizer(in_val, n_bits, xmax, m)
     L = 2 .^ n_bits;
     delta = (2 * xmax) / L;
-    levels = -xmax:delta:xmax;
-    q_ind = max(min(floor((in_val + xmax - ((m) * delta / 2))/delta) + 1,length(levels) - (1-m)*1),1);
+    q_ind = max(min(floor((in_val + xmax - ((m) * delta / 2))/delta) + 1,L),1);
 
 end
